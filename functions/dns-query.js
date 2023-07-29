7f5d369207d8962dc5bb776242172f0cc6dfc596
jingchengdev <90081101+jingchengdev@users.noreply.github.com>
2023-07-29 17:57:10 +0800

    Update dns-query.js

--- functions/dns-query.js
+++ functions/dns-query.js
@@ -1,5 +1,5 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://dns.controld.com/'
+const dohjson = 'https://dns.controld.com/'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
